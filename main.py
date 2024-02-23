ae04e482f7de24207af8a2b544cf972fc705e035
Kevin Chavez <kjc47@njit.edu>
2024-02-23 12:39:11 -0500

    fix

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from app import App
 # You must put this in your main.py because this forces the program to start when you run it from the command line.
 if __name__ == "__main__":
     app = App().start()  # Instantiate an instance of App
-=======
+
 if __name__ == "__main__":
     App.start()
